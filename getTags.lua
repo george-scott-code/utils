99ef52055bf87a31a6124744a0ceaed0dfb9156f
geo <george.scott.code@gmail.com>
2025-04-22 12:45:51 +0100

    handle user input properly

--- getTags.lua
+++ getTags.lua
@@ -2,13 +2,18 @@ local filename = arg[1]
 
 if not filename then 
     print("enter filename:")
-    fileName = io.read()
+    filename = io.read()
 end
 
-io.input(filename)
-local content = io.read("*all")
-
-for tag in string.gmatch(content, "(#%a+)") do
-    print(tag)
+handle, error = io.open(filename)
+if handle == nil then
+    print(error)
+else
+    io.input(handle)
+    
+    local content = io.read("*all")
+    
+    for tag in string.gmatch(content, "(#%a+)") do
+        print(tag)
+    end
 end
-
